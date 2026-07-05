3d7c1364e449cb3b31243e2c87f1bed413153f25
Asmara_Kusuma <iasmarakusuma@gmail.com>
2026-07-05 15:07:16 +0800

    feat(database): tambah setup migrasi skema supabase otomatis via npm dan CLI

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -7,6 +7,7 @@ class AppConstants {
   static const String keyUserName = 'user_name';
 
   // Supabase Configuration
-  static const String supabaseUrl = 'YOUR_SUPABASE_URL';
-  static const String supabasePublishableKey = 'YOUR_SUPABASE_PUBLISHABLE_KEY';
+  static const String supabaseUrl = 'https://whcabbztpbsvuzlupqda.supabase.co';
+  static const String supabasePublishableKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoY2FiYnp0cGJzdnV6bHVwcWRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyMjQ1MDUsImV4cCI6MjA5ODgwMDUwNX0.DfLA9xCxQsClBUnQwuQ9l7n-uvyJSJAIsz4gVpIEO8M';
 }
